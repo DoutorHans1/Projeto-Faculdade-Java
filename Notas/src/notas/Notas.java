03014a250c35b0abfe33e89918fc048dc13e3593
DoutorHans1 <136267567+DoutorHans1@users.noreply.github.com>
2024-03-18 20:43:27 -0300

    Add files via upload

---
+++ Notas/src/notas/Notas.java
@@ -0,0 +1,36 @@
+
+package notas;
+
+import java.util.Scanner;
+
+
+public class Notas {
+
+
+    public static void main(String[] args) {
+    
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno; ");
+        String nomeAluno = ler.nextLine();
+        
+        double n1, n2, n3, n4;
+
+        System.out.println("Digite a primeira nota: ");
+        n1 = ler.nextDouble();
+        
+        System.out.println("Digite a segunda nota: ");
+        n2 = ler.nextDouble();
+        
+        System.out.println("Digite a terceira nota: ");
+        n3 = ler.nextDouble();
+        
+        System.out.println("Digite a quarta nota: ");
+        n4 = ler.nextDouble();
+        
+        double media = (n1 + n2 + n3 + n4) / 4;
+        
+        System.out.println("A media de " + nomeAluno + " e " + media);
+        
+    }
+    
+}
---
+++ Notas/src/notas/NumeroMaior.java
@@ -0,0 +1,35 @@
+
+package notas;
+
+import java.util.Scanner;
+
+public class NumeroMaior {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Sistema de identificar o maior numero");
+        
+        double n1, n2;
+        
+        System.out.println("Digite um numero: ");
+        n1 = ler.nextDouble();
+        
+        System.out.println("Digite outro numero: ");
+        n2 = ler.nextDouble();
+        
+        String numeroV = "";
+        
+        while(!numeroV.equalsIgnoreCase("s")){
+        
+        if (n1 > n2){
+            System.out.println("O numero maior entre os numeros " + n1 + " e " + n2 + " e: " + n1);
+        } else if(n1 == n2){
+            System.out.println("Os numeros sao iguais." + n1 + " e " + n2);
+        } else {
+            System.out.println("O numero maior entre os numeros " + n1 + " e " + n2 + " e: " + n2);
+        }
+        
+        System.out.println("Voce deseja sair? Se sim, tecle s: ");
+        numeroV = ler.next();
+    }
+    }
+}
