07d497855dbe435e920b604d9fb16e4066705f15
DoutorHans1 <136267567+DoutorHans1@users.noreply.github.com>
2024-03-19 21:30:43 -0300

    Add files via upload

---
+++ ConversaoBinario/Identificacao/src/identificacao/Identificacao.java
@@ -0,0 +1,32 @@
+
+package identificacao;
+
+import java.util.Scanner;
+
+
+public class Identificacao {
+  
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        String nome, idade, cidade, prof;
+        
+        
+        System.out.println("Digite seu nome: ");
+        nome = ler.nextLine();
+        
+        System.out.println("Digite ssua idade: ");
+        idade = ler.nextLine();
+        
+        System.out.println("Digite sua cidade: ");
+        cidade = ler.nextLine();
+        
+        System.out.println("Digite o nome do professor(a): ");
+        prof = ler.nextLine();
+        
+        System.out.println("Olá, meu nome é " + nome + " tenho " + idade + " anos e sou da cidade de "
+                 + cidade + ". Estou aprendendo a programar com o(a) Prof. " + prof );
+
+    }
+    
+}
