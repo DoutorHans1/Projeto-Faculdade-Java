9d6b73ab89dfb2dfe21bb892c50abbcc7276ab13
DoutorHans1 <136267567+DoutorHans1@users.noreply.github.com>
2024-03-24 17:59:17 -0300

    Add files via upload

---
+++ CalcularMedia.java
@@ -0,0 +1,28 @@
+
+package faculdadeages;
+
+import java.util.Scanner;
+
+public class CalcularMedia {
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner(System.in);
+
+        double nota1, nota2, nota3, media;
+        
+        System.out.println("Digite a primeira nota: ");
+        nota1 = ler.nextDouble();
+        System.out.println("Digite a segunda nota: ");
+        nota2 = ler.nextDouble();
+        System.out.println("Digite a terceira nota: ");
+        nota3 = ler.nextDouble();
+        
+        media = (nota1 + nota2 + nota3) / 3;
+        
+        if (media >= 7){
+            System.out.println("Aprovado.");             
+        } else {
+            System.out.println("Reprovado.");
+        } 
+    }
+}
---
+++ CalculoIMC.java
@@ -0,0 +1,30 @@
+
+package faculdadeages;
+
+import java.util.Scanner;
+
+public class CalculoIMC {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);        
+        double peso, altura, imc;
+        
+        System.out.println("Digite seu peso: ");
+        peso = ler.nextDouble();
+        System.out.println("Digite sua altura: ");
+        altura = ler.nextDouble();
+        
+        imc = peso / (altura * altura);
+        
+        System.out.println("Seu IMC é: " + imc);
+        
+        if (imc <= 18.5){
+            System.out.println("Voce está abaixo do peso");
+        } else if(imc <= 24.9){
+            System.out.println("Voce está com o peso ideal.");
+        } else if(imc <= 29.9){
+            System.out.println("Voce está com sobrepeso.");
+        } else if(imc >= 30){
+            System.out.println("Voce está obeso.");
+        }  
+    }
+}
